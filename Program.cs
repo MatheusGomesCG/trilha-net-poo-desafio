3fed386b82168f5ce43705fc6f6769f76c308052
Matheus Gomes F. Eusebio <matt-xp2@live.com>
2025-05-22 08:49:26 -0300

    Foi realizado as implementações necessárias para conclusão do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Sony Erickson", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
